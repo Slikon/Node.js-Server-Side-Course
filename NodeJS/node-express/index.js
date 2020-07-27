f19bcbc77ebb3c61bf56be0cb6325f8ef4a0328b
Slikon <slava.kulik27@gmail.com>
2020-07-27 15:14:52 +0300

    added: Nodemon; added: promoRouter, leaderRouter, /:dishId param support in dishRouter

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,42 +3,31 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-//Express router
-app.use('/dishes', dishRouter);
-  
-  app.get('/dishes/:dishId', (req,res,next) => {
-      res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-  });
-  
-  app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-  });
-  
-  app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-          ' with details: ' + req.body.description);
-  });
-  
-  app.delete('/dishes/:dishId', (req, res, next) => {
-      res.end('Deleting dish: ' + req.params.dishId);
-  });
+//serve static
+app.use(express.static(__dirname + '/public'))
 
+//Express dishRouter
+app.use('/dishes', dishRouter);
+
+//Express promoRouter
+app.use('/promotions', promoRouter)
+
+//Express leaderRouter
+app.use('/leaders', leaderRouter)
+  
 //use morgan for logging
 app.use(morgan('dev'))
 
 //use body-parser for json parsing from request body
 app.use(bodyParser.json())
-
-//serve static
-app.use(express.static(__dirname + '/public'))
 
 const server = http.createServer(app);
 
