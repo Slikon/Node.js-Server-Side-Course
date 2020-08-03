0187d7662accbf8373a87c1fdd89b25da31baa15
Slikon <slava.kulik27@gmail.com>
2020-08-04 01:09:47 +0300

    Node Mongoose module Example #1

---
+++ NodeJS/node-mongoose/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+const { db } = require('./models/dishes')
+
+const url ='mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url, { useNewUrlParser: true })
+
+connect.then((db) => {
+    console.log('Succesfully connected to the server with url ' + url);
+
+    let newDish = Dishes({
+        name: 'PayVay',
+        description: 'test pizzeria'
+    })
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish + '\tsuccesfully saved');
+
+            return Dishes.find({}).exec()
+        })
+        .then((dishes) =>{
+            console.log(dishes);
+
+            return Dishes.remove({})
+        })
+        .then(() => {
+            return mongoose.connection.close()
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
